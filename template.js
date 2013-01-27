40c73fbee85d062a82a9782db45604ea78b84cf9
Matthew Phillips <matthew@matthewphillips.info>
2013-01-27 10:50:15 -0500

    Forgot to make

---
+++ template.js
@@ -0,0 +1,1 @@
+module.exports = '<div class="filepicker-mycomputer">\n  <div class="filepicker-mycomputer-drop">\n  </div>\n  <div>\n    <button class="filepicker-mycomputer-select">Select</button>\n  </div>\n</div>\n';
